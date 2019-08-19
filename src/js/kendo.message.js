55a5fd0d880a1926212b2a84e6f8f0bf04e375ea
John Rummell <john@rummellweb.com>
2019-08-18 20:48:08 -0400

    lint fixes

--- src/js/kendo.message.js
+++ src/js/kendo.message.js
@@ -1,57 +1,43 @@
-/*
-    Message plugin for Kendo UI Web
-    Copyright (c) 2013 John Rummell (jrummell.com)
-    Licensed under the GPL license (http://www.gnu.org/licenses/gpl.html)
-    Version: _VERSION_
+/**
+    @description Message plugin for Kendo UI Web
+    @copyright 2013 John Rummell (jrummell.com)
+    @license GPL license (http://www.gnu.org/licenses/gpl.html)
+    @version: _VERSION_
 */
-
-kendo_module({
-    id: "message",
-    name: "Message",
-    category: "web",
-    description: "The Message widget renders styled messages.",
-    depends: [ "core" ]
-});
 
 //
 // create closure
 //
-(function (jQuery)
-{
+(function (jQuery) {
     //
     // plugin methods
     //
     var methods = {
-        init: function (options)
-        {
+        init: function (options) {
             options = jQuery.extend({}, jQuery.fn.kendoMessage.defaults, options);
 
-            return this.each(function ()
-            {
+            return this.each(function () {
                 var $this = jQuery(this);
                 var data = $this.data("message");
 
                 // only initialize once
-                if (!data)
-                {
+                if (!data) {
                     // use given message or inner html
                     var messageText = options.message;
-                    if (messageText == null || messageText == "")
-                    {
+                    if (messageText == null || messageText === "") {
                         messageText = $this.html();
                     }
 
                     // info or error?
-                    var messageClass = options.type == "info" ? "k-block k-info-colored" : "k-block k-error-colored";
-                    var iconClass = options.type == "info" ? "k-i-tick" : "k-i-note";
+                    var messageClass = options.type === "info" ? "k-block k-info-colored" : "k-block k-error-colored";
+                    var iconClass = options.type === "info" ? "k-i-tick" : "k-i-note";
 
                     // build message html
                     var messageHtml = "<div class='k-content message-container'>";
                     messageHtml += "<div class='" + messageClass + " ui-corner-all' >";
                     messageHtml += "<p><span class='k-icon " + iconClass + "' style='float:left;'></span>";
                     messageHtml += "<span class='message-text'>" + messageText + "</span>";
-                    if (options.dismiss)
-                    {
+                    if (options.dismiss) {
                         messageHtml += "<a class='message-dismiss' href='#'>Dismiss</a>";
                     }
                     messageHtml += "</p></div></div>";
@@ -59,18 +45,15 @@ kendo_module({
                     // set html and show the message
                     $this.html(messageHtml);
 
-                    if (options.dismiss)
-                    {
+                    if (options.dismiss) {
                         // hide messages on click
-                        jQuery(".message-dismiss", $this).click(function ()
-                        {
-                            $this.hide('normal');
+                        jQuery(".message-dismiss", $this).click(function () {
+                            $this.hide("normal");
                             return false;
                         });
                     }
-                    
-                    if (options.autoShow)
-                    {
+
+                    if (options.autoShow) {
                         $this.show();
                     }
 
@@ -79,28 +62,22 @@ kendo_module({
                 }
             });
         },
-        options: function (options)
-        {
-            return this.each(function ()
-            {
+        options: function (options) {
+            return this.each(function () {
                 var $this = jQuery(this);
                 var currentOptions = $this.data("message") || {};
                 options = jQuery.extend({}, currentOptions, options);
                 $this.kendoMessage("destroy").kendoMessage("init", options);
             });
         },
-        show: function ()
-        {
+        show: function () {
             jQuery(this).show();
         },
-        hide: function ()
-        {
+        hide: function () {
             jQuery(this).hide();
         },
-        destroy: function ()
-        {
-            return this.each(function ()
-            {
+        destroy: function () {
+            return this.each(function () {
                 var $this = jQuery(this);
                 var data = $this.data("message");
 
@@ -111,20 +88,19 @@ kendo_module({
         }
     };
 
-    jQuery.fn.kendoMessage = function (method)
-    {
-        if (methods[method])
-        {
+    jQuery.fn.kendoMessage = function (method) {
+        if (methods[method]) {
+            // eslint-disable-next-line prefer-rest-params
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         }
-        else if (typeof (method) === 'object' || !method)
-        {
+        if (typeof (method) === "object" || !method) {
+            // eslint-disable-next-line prefer-rest-params
             return methods.init.apply(this, arguments);
         }
-        else
-        {
-            jQuery.error("Method " + method + " does not exist on jQuery.kendoMessage");
-        }
+
+        jQuery.error("Method " + method + " does not exist on jQuery.kendoMessage");
+
+        return null;
     };
 
     //
@@ -136,4 +112,4 @@ kendo_module({
         dismiss: true, // append 'Click to dismiss' to message and hide on click
         autoShow: true // show on initialize
     };
-})(jQuery);
+}(jQuery));
